8743d83835765a994f06d8af0497b54ad674392a
Iraquitan Cordeiro Filho <iraquitanfilho@gmail.com>
2019-02-07 09:29:42 -0300

    fix: Update due to security issues

--- README.md
+++ README.md
@@ -1,44 +1,52 @@
-[![Code Climate](https://codeclimate.com/github/iraquitan/iraquitan.github.io/badges/gpa.svg)](https://codeclimate.com/github/iraquitan/iraquitan.github.io)
 # iraquitan.github.io
+
+[![Code Climate](https://codeclimate.com/github/iraquitan/iraquitan.github.io/badges/gpa.svg)](https://codeclimate.com/github/iraquitan/iraquitan.github.io)
+
 My personal GitHub page built with Jekyll.
 
 ## Table of contents
+
 * [Requirements](#requirements)
 * [Quick start](#quick-start)
 * [Creator](#creator)
 * [License](#license)
 
 ## Requirements
+
 * Git
 * [Ruby](https://www.ruby-lang.org/en/)
 * [Bundler](http://bundler.io/)
 * [Jekyll](https://jekyllrb.com/)
 
 ## Quick start
+
 * Clone this repo `git clone https://github.com/iraquitan/iraquitan.github.io.git your-folder`.
 * Change to the `/your-folder` directory.
 * Run `bundle install` to install dependencies
 * To test locally, you can run `bundle exec jekyll serve`
 * To customize to your own data, you must change some fields in **_config.yml**.
-    * _title_: Change to your new site title.
-    * _email_: Change to your own email.
-    * _description_: Change to your own site description.
-    * _url_: Change to your own github user page repository `http://github-user.github.io`.
-    * _timezone_: Change to your own timezone following this [list](https://www.wikiwand.com/en/List_of_tz_database_time_zones).
+  * _title_: Change to your new site title.
+  * _email_: Change to your own email.
+  * _description_: Change to your own site description.
+  * _url_: Change to your own github user page repository `http://github-user.github.io`.
+  * _timezone_: Change to your own timezone following this [list](https://www.wikiwand.com/en/List_of_tz_database_time_zones).
 * To customize to your own data, you must change some fields in *_data* folder.
-    * **people.yml**: Change to your own name, twitter handle, github user and linkedin user.
-    * **profiles.yml**: Change the _username_ field in all items to your own profiles.
+  * **people.yml**: Change to your own name, twitter handle, github user and linkedin user.
+  * **profiles.yml**: Change the _username_ field in all items to your own profiles.
 * To customize the favicon and brand image, you can generate yout onw favicon [here](http://realfavicongenerator.net/) and replace the contents on the *assets/img/favicons* folder
 * To deploy, you need the following steps:
-    * Remove the current remote by running `git remote rm origin`.
-    * Then add your own github pages repository remote by running `git remote add origin https://github.com/user/repo.git`.
-    * Then commit your customization changes and push to your remote master branch.
+  * Remove the current remote by running `git remote rm origin`.
+  * Then add your own github pages repository remote by running `git remote add origin https://github.com/user/repo.git`.
+  * Then commit your customization changes and push to your remote master branch.
 
 ## Creator
-**Iraquitan Cordeiro Filho**
+
+### Iraquitan Cordeiro Filho
+
 * <https://github.com/iraquitan>
 * <https://www.linkedin.com/in/iraquitan>
 * <https://twitter.com/iraquitan_filho>
 
 ## License
+
 The contents of this repository are covered under the [MIT License](LICENSE).
--- gulpfile.js
+++ gulpfile.js
@@ -4,24 +4,20 @@
 /* eslint-env node */
 /* eslint-disable no-console */
 
-var gulp = require("gulp");
-var sass = require("gulp-sass");
-var sourceMaps = require("gulp-sourcemaps");
-var jekyll = require("gulp-jekyll-stream");
-var ghPages = require("gulp-gh-pages");
-var autoprefixer = require("gulp-autoprefixer");
-var imagemin = require("gulp-imagemin");
-var eslint = require("gulp-eslint");
-var concat = require("gulp-concat");
-var uglify = require("gulp-uglify");
-var pump = require("pump");
+const { src, dest, parallel, series } = require('gulp');
+const sass = require("gulp-sass");
+const sourcemaps = require("gulp-sourcemaps");
+const jekyll = require("gulp-jekyll-stream");
+const ghPages = require("gulp-gh-pages");
+const autoprefixer = require("gulp-autoprefixer");
+const imagemin = require("gulp-imagemin");
+const eslint = require("gulp-eslint");
+const concat = require("gulp-concat");
+const uglify = require("gulp-uglify");
 
-gulp.task("default", function () {
-    console.log("Gulp running...");
-});
 
-gulp.task("jekyll", function () {
-    return gulp.src(process.cwd())
+function jekyll_task() {
+    return src(process.cwd())
         .pipe(jekyll({
             bundleExec: true,              // exec jekyll w/ "bundle exec"
             quiet: true,                   // suppress jekyll output; implies "--trace"
@@ -32,67 +28,55 @@ gulp.task("jekyll", function () {
             // source: '/path/to/source'   // overrides gulp.src() above
             // destination: '_site'        // can be used instead of gulp.dest()
         }))
-        .pipe(gulp.dest("_site"));
-});
+        .pipe(dest("_site"));
+}
 
-gulp.task("dist", ["jekyll", "styles", "imagemin", "lint", "scripts-dist"], function () {});
-
-gulp.task("gh-pages", function () {
-    return gulp.src("_site/**/*")
+function ghp() {
+    return src("_site/**/*")
         .pipe(ghPages({
             branch: "master",
             force: true
-        }));
-});
+        }))
+}
 
-gulp.task("deploy", ["dist", "gh-pages"], function () {
-    console.log("deploy task done.");
-});
-
-gulp.task("styles", function () {
-    gulp.src("_sass/**/*.scss")
-        .pipe(sourceMaps.init())
+function css() {
+    return src("_sass/**/*.scss")
+        .pipe(sourcemaps.init())
         .pipe(sass({outputStyle: "compressed"}).on("error", sass.logError))
         .pipe(autoprefixer({
             browsers: ["last 2 versions"]
         }))
-        .pipe(sourceMaps.write())
-        .pipe(gulp.dest("_site/assets/css"));
-});
+        .pipe(sourcemaps.write())
+        .pipe(dest("_site/assets/css"));
+}
 
-gulp.task("imagemin", function () {
-    gulp.src("assets/img/**/*")
+function img() {
+    src("assets/img/**/*")
         .pipe(imagemin({
             progressive: true
         }))
-        .pipe(gulp.dest("_site/assets/img"));
-});
+        .pipe(dest("_site/assets/img"));
+}
 
-gulp.task("scripts", function () {
-    pump([
-        gulp.src("assets/js/*.js"),
-        concat("all.js"),
-        gulp.dest("_site/assets/js")
-    ]);
-});
+function js() {
+    return src("assets/js/*.js", { sourcemaps: true })
+        .pipe(concat("all.js"))
+        .pipe(dest("_site/assets/js"))
+}
 
-gulp.task("scripts-dist", function () {
-    pump([
-        gulp.src("assets/js/*.js"),
-        sourceMaps.init(),
-        concat("all.js"),
-        uglify(),
-        sourceMaps.write(),
-        gulp.dest("_site/assets/js")
-    ]);
-});
+function jsDist() {
+    return src("assets/js/*.js", { sourcemaps: true })
+        .pipe(concat("all.js"))
+        .pipe(uglify())
+        .pipe(dest("_site/assets/js"))
+}
 
-gulp.task("lint", function () {
+function lint() {
     // ESLint ignores files with "node_modules" paths.
     // So, it's best to have gulp ignore the directory as well.
     // Also, Be sure to return the stream from the task;
     // Otherwise, the task may end before the stream has finished.
-    return gulp.src(["assets/js/**/*.js"])
+    return src(["assets/js/**/*.js"])
     // eslint() attaches the lint output to the "eslint" property
     // of the file object so it can be used by other modules.
         .pipe(eslint())
@@ -102,4 +86,10 @@ gulp.task("lint", function () {
     // To have the process exit with an error code (1) on
     // lint error, return the stream and pipe to failAfterError last.
     // .pipe(eslint.failAfterError());
-});
+}
+
+exports.css = css;
+exports.js = js;
+exports.img = img;
+exports.dist = series(jekyll_task, css, img, lint, jsDist);
+// exports.deploy = series(di/st, ghp);
